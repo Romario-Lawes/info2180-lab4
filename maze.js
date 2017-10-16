aa0c5dc34f0793420b8fa90baf648a074cc666d9
Lawes <lawesromario@yahoo.com>
2017-10-16 17:59:01 -0500

    Exercise 2

--- maze.js
+++ maze.js
@@ -4,4 +4,13 @@ window.onload = function () {
   document.getElementById("boundary1").onmouseover = function () {
     this.className += " youlose";
   };
+
+  let wall = document.getElementsByClassName("boundary");
+
+  for (let i = 0; i < wall.length; i++) {
+      wall[i].onmouseover = function () {
+        this.className += " youlose";
+      };
+  }
+  
 };
